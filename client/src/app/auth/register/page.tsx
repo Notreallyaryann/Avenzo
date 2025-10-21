12dface21dc91653e5ae227d70d20eaac66139e4
Aryan <vedprakashverma4842@gmail.com>
2025-10-21 11:19:00 +0530

    completed auth pages

---
+++ client/src/app/auth/register/page.tsx
@@ -0,0 +1,95 @@
+import Image from 'next/image'
+import banner from '../../../../public/banner2.jpg'
+import logo from '../../../../public/logo1.png'
+
+import React from 'react'
+import { Label } from '@/components/ui/label'
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
+
+const Registerpage = () => {
+  return (
+    <div className="min-h-screen bg-[#fff6f4] flex">
+
+      <div className="hidden lg:block w-1/2 bg-[#ffede1] relative overflow-hidden">
+        <Image
+          src={banner}
+          alt="Register"
+          fill
+          style={{ objectFit: 'cover', objectPosition: 'center' }}
+          priority
+        />
+      </div>
+      <div className='w-full lg:w-1/2 flex flex-col p-8 lg:p-16 justify-center'>
+      <div className='max-w-md w-full mx-auto'>
+        <div className='flex justify-center'>
+
+         <Image src={logo} width={200} height={50} alt="Logo" />
+        </div>
+<form className='space-y-4'>
+
+  <div className='space-y-1'>
+    <Label htmlFor='name'>Full Name</Label>
+    <Input
+    id="name"
+    name="name"
+    type="text"
+    placeholder='Enter Your Name'
+      className='bg-[#ffede1]'
+    required
+    
+    />
+
+
+  </div>
+  <div className='space-y-1'>
+    <Label htmlFor='name'>Email</Label>
+    <Input
+    id="email"
+    name="email"
+    type="email"
+    placeholder='Enter Your Email'
+    required
+    className='bg-[#ffede1]'
+    />
+
+
+  </div>
+  <div className='space-y-1'>
+    <Label htmlFor='name'>Password</Label>
+    <Input
+    id="password"
+    name="password"
+    type="text"
+        className='bg-[#ffede1]'
+    placeholder='Enter Your Password'
+    required
+    
+    />
+
+
+  </div>
+  <Button type='submit' className='w-full bg-black text-white hover:bg-black transition-colors'>
+    CREATE ACCOUNT
+
+  </Button>
+      <p className="text-center text-[#3f3d56] text-sm">
+              Already have an account{" "}
+              <Link
+                href={"/auth/login"}
+                className="text-[#000] hover:underline font-bold"
+              >
+                Sign In
+              </Link>
+            </p>
+</form>
+
+      </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Registerpage
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,40 @@
 import { PrismaClient } from "@prisma/client";
+import dotenv from 'dotenv'
+import express from 'express'
+import cors from 'cors'
+import cookieParser  from "cookie-parser";
+import authRoutes from './routes/authRoutes'
+//env variables
+dotenv.config()
+const app=express()
+const PORT=process.env.PORT || 3001
 
+const corsOptions={
+    origin:'http://localhost:3000',
+    Credentials:true,
+    methods:['GET','PUT','POST','DELETE','OPTIONS'],
+    allowedHeaders:['content-Type','Authorization'],
 
+}
 
+app.use(cors(corsOptions))
+app.use(express.json())
+app.use(cookieParser())
 
 
 
 export const prisma =new PrismaClient()
+
+app.use('/api/auth',authRoutes)
+app.get('/',(req,res)=>{
+    res.send("Hello!!")
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on PORT :${PORT}`)
+})
+
+process.on("SIGINT", async () => {
+  await prisma.$disconnect();
+  process.exit();
+});
