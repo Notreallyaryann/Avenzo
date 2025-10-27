eb1b116946b59e1faeacb34be01f83871c7d4e38
Aryan <vedprakashverma4842@gmail.com>
2025-10-27 15:11:58 +0530

    super admin

---
+++ client/src/app/super-admin/layout.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import React, { useState } from 'react'
+
+const SuperAdminLayout = () => {
+    const [isSidebarOpen,setIsSidebarOpen]=useState(true)
+  return (
+    <div className='min-h-screen bg-background'>
+    <div></div>
+    </div>
+  )
+}
+
+export default SuperAdminLayout
